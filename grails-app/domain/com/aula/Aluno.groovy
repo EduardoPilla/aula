1e4ff600ce95bd2a377aa19176c59555eda0f512
unknown <eduardo@clicksistemas.com.br>
2017-06-24 02:51:04 -0300

    Incluido o atributo RA conforme requisito

--- grails-app/domain/com/aula/Aluno.groovy
+++ grails-app/domain/com/aula/Aluno.groovy
@@ -3,9 +3,11 @@ package com.aula
 class Aluno {
     String nome
     String email
+    String ra
 
     static constraints = {
         nome blank:false, size:1..30
         email blank:false, email:true
+        ra blank:false
     }
 }
